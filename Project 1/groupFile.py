5ba1495a1929ab5d2efb925fe37046ecf746c90a
Boden Kahn <74627195+Boden7@users.noreply.github.com>
2024-10-11 08:49:07 -0400

    Update groupFile.py
    Fixed spelling because I'm goofy

--- Project 1/groupFile.py
+++ Project 1/groupFile.py
@@ -209,23 +209,23 @@ class BankAccount:
        # If anything was wrong with the amount parameter the transaction will be rejected
        return False
 
-   # Withdrawls money from the account if the transaction is valid and records the transaction; 
+   # Withdrawals money from the account if the transaction is valid and records the transaction; 
    # If the transaction is valid but the account will be overdrawn, applies an overdraft fee and 
    # updates the counter for overdraws
    #@param amount: the amount to be withdrawn
    #@require amount > 0
-   #@return The success or failure of the withdrawl
-   def withdrawl(self, amount):
-       # Make sure the amount to withdrawl is not negative
+   #@return The success or failure of the withdrawal
+   def withdrawal(self, amount):
+       # Make sure the amount to withdrawal is not negative
        if isinstance(amount, float) and amount <= 0:
            print("Transaction denied.")
-       # Ensure the balance is at least $250 more than the withdrawl amount
+       # Ensure the balance is at least $250 more than the withdrawal amount
        if isinstance(amount, float) and amount < self.balance + 250.0:
            # Process the transaction and update necessary variables
-           withdrawlTransaction = Transaction("withdrawl", amount)
-           self._accountTransactions.append(withdrawlTransaction)
+           withdrawalTransaction = Transaction("withdrawal", amount)
+           self._accountTransactions.append(withdrawalTransaction)
            self.balance -= amount
-           # If the withdrawl would put the balance in the negative, add an
+           # If the withdrawal would put the balance in the negative, add an
            # overdraft fee and increment the overdrawn counter
            if self.balance < 0.0:
                # Process the transaction and update necessary variables
